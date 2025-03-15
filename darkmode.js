efbd8da046e00df56b4c5a1ea13f64a05b6b1db0
Yusdevlop <yusifdevlopercode154@gmail.com>
2025-03-15 15:40:51 +0400

    fixed

--- darkmode.js
+++ darkmode.js
@@ -10,7 +10,9 @@ const section5box1text = document.querySelector(".section5box1text");
 const section5box2text = document.querySelector(".section5box2text");
 const section5box3text = document.querySelector(".section5box3text");
 const section5box4text = document.querySelector(".section5box4text");
-
+const lighticonlink = document.querySelector(".lighticonlink");
+const darkiconlink = document.querySelector(".darkiconlink");
+const iconclass =document.querySelector(".material-symbols-outlined")
 
 darkmodebtn.addEventListener("click",()=>{
     darkmodebtn.classList.toggle("dark");
@@ -25,7 +27,7 @@ darkmodebtn.addEventListener("click",()=>{
         section5box2text.style=`color:#080808`
         section5box3text.style=`color:#080808`
         section5box4text.style=`color:#080808`
-
+        iconclass.innerHTML("dark_mode");
         
     }else{
         section1.style =`background-color:#080808  ; `;
@@ -34,7 +36,7 @@ darkmodebtn.addEventListener("click",()=>{
         section5title.style=`color:#ffff`;
         header.style=`background-color:#080808`;
         section5titletextalt.style=`color:#080808`;
-        section5box1text.style=`color:#080808`;
+        section5box1text.style=`color:#ffff`;
         section5box2text.style=`color:#ffff`;
         section5box3text.style=`color:#ffffff`;
         section5box4text.style=`color:#fffff`;
---
+++ data.js
@@ -0,0 +1,34 @@
+const data=[
+    {
+        image:"https://anea.az/assets/img/logo/logo-en.png",
+        text:"Anea.az",
+        link:"Anea.az"
+    },
+    {
+        image:"https://weather-web-site-gamma.vercel.app/src/img/weather02-512.webp",
+        text:"Weather Website",
+        link:"weather-web-site-gamma.vercel.app"
+    }
+
+
+  
+]
+
+
+const section6 = document.querySelector(".section6rightleft");
+section6.innerHTML = data.map(
+    (item) => `
+    <div class="section6right">
+        <img src="${item.image}" alt="" class="section6rightimg">
+        <p class="section6righttitletext">${item.text}</p>
+        <a href="https://${item.link}" target="_blank">
+            <button class="section6rightbtn">Know more ></button>
+        </a>
+    </div>`
+).join("");
+
+
+
+
+
+// section6right
