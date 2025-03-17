cbeb8945df04fda43974e1afd3392c170cd68af9
Yusdevlop <yusifdevlopercode154@gmail.com>
2025-03-17 20:49:43 +0400

    fixed

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data=[
     {
         image:"https://i.pinimg.com/originals/d8/10/55/d81055d32cf810a6d527b5eb9ae20f08.png",
         text:"Search Program",
-        link:"https://search-program.vercel.app/"
+        link:"git search-program.vercel.app/"
     },
     
     {
